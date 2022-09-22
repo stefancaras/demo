287123bda94fed34a5f20b535ed7cd9bab0b3774
Stefan Caras <stefan.caras@gmail.com>
2022-09-22 13:11:05 +0300

    update

--- x/script.js
+++ x/script.js
@@ -1,20 +1,22 @@
 const createCardFromProduct = (product) => {
-	return `<div class='card'>
-              <h3>${product.author}</h3>
-              <img src='${product.book_image}' />
-              <p>${product.price} Lei</p>
+	return `<div class='card flex column'>
+              <img class="bookImage" src='${product.book_image}' />
+			  <p class="title blackText"><b>${product.title.toLowerCase()}</b></p>
+			  <p class="blackText">by ${product.author}</p>
+			  <p class="description blackText">${product.description}</p>
+              <p class="price">$ ${product.price}</p>
           </div>`;
 };
 
 const getProductsOnIndexPage = () => {
-	fetch('./nonfiction.json')
+	fetch('./json/fiction.json')
 		.then((result) => result.json())
 		.then((products) => {
 			const productCards = products.map((product) =>
 				createCardFromProduct(product)
 			);
 			const innerHTMLProducts = productCards.join('');
-			document.querySelector('.container').innerHTML =
+			document.querySelector('#container').innerHTML =
 				innerHTMLProducts;
 		});
 };
