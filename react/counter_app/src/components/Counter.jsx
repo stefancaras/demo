64f8ae235b07dbb0217753174fa14d8305ab4dd2
Stefan Caras <stefan.caras@gmail.com>
2022-12-28 18:15:12 +0200

    update

--- react/counter_app/src/components/Counter.jsx
+++ react/counter_app/src/components/Counter.jsx
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 export default function Counter() {
   let [count, setCount] = useState(0);
   return (
-    <div className="text-center">
-      <h1 className="my-5">Counter app</h1>
+    <div className="text-center h1">
+      <p className="my-5">Counter app</p>
       <span onClick={() => setCount(--count)}>
-				<i className="bi bi-dash-square h1 text-danger"></i>
+        <i className="bi bi-dash-square text-danger"></i>
       </span>
-      <span className="h1 mx-3">{count}</span>
-			<span onClick={() => setCount(++count)}>
-				<i className="bi bi-plus-square h1 text-success"></i>
+      <span className="mx-3">{count}</span>
+      <span onClick={() => setCount(++count)}>
+        <i className="bi bi-plus-square text-success"></i>
       </span>
     </div>
   );
