4e85c6e8048b9f9611cc78c0b2d83c1643462dad
Stefan Caras <stefan.caras@gmail.com>
2023-01-10 20:31:29 +0200

    moved react

--- react/counter_app/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import * as ReactDOMClient from 'react-dom/client';
-import Counter from './components/Counter'
-
-ReactDOMClient.createRoot(document.querySelector("#root")).render(<Counter />)
--- react/whatever/src/index.js
+++
@@ -1,7 +0,0 @@
-import * as ReactDOMClient from 'react-dom/client';
-import App from "./App"
-import "./style.css"
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
-
-ReactDOMClient.createRoot(document.querySelector("#root")).render(<App />)
