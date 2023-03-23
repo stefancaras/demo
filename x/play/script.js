012190d54f47c231607164c135be1938f4a4296d
Stefan Caras <stefan.caras@gmail.com>
2023-03-23 21:34:56 +0200

    contact2

--- x/play/script.js
+++
@@ -1,8 +0,0 @@
-const panels = document.querySelectorAll(".panel");
-
-document.querySelector(".container").addEventListener("click", (event) => {
-  if (event.target.classList.contains("panel")) {
-    panels.forEach((panel) => panel.classList.remove("active"));
-    event.target.classList.add("active");
-  }
-});
