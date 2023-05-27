5c3e0e1c623d74548bfa4f7579f58ec0dd95c3e5
Stefan Caras <stefan.caras@gmail.com>
2023-05-27 08:33:02 +0300

    update

--- x/stringChallenge2.js
+++
@@ -1,8 +0,0 @@
-const StringChallenge = (str, num) => {
-  const result = [];
-  for (let i = 0; i < str.length; i++) {
-    result.push(String.fromCharCode(str.charCodeAt(i) + num));
-  }
-  return result.join("");
-};
-console.log(StringChallenge("Hello", 4));
