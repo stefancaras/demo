5cf85f2c1a187232726f1f433af1bde9ae0e0b08
Stefan <108738092+stefancaras@users.noreply.github.com>
2025-01-08 09:02:06 +0200

    Update eve.js

--- JS/eve.js
+++ JS/eve.js
@@ -1,12 +1,4 @@
-let prev = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let curr = "";
-let gen = 0;
-while (prev[0] !== prev.at(-1)) {
-  curr = "";
-  for (let i = 0; i < prev.length; i++) {
-    curr += prev[i].repeat(Math.floor(Math.random() * 3));
-  }
-  prev = curr;
-  gen++;
-  console.log(gen, prev);
-}
+let x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+for (let i = 1; x[0] !== x.at(-1); i++)
+  console.log(i, (x = x.replace(/./g, (e) => e.repeat(~~(Math.random() * 3)))));
